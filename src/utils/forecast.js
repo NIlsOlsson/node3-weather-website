e72dc568bb1049d72b40453d75007e896056f9df
Nils Olsson <niolsson@gmail.com>
2020-07-02 16:07:23 +0200

    Add some more weather information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) { 
             callback("Unable to find location", undefined) 
         }else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out. The humidity is " + body.current.humidity + "%. The wind direction is " + body.current.wind_dir + " and the wind speed is " + body.current.wind_speed + " m/s.")
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out and it feels like " + body.current.feelslike + " . The humidity is " + body.current.humidity + "%. The wind direction is " + body.current.wind_dir + " and the wind speed is " + body.current.wind_speed + " m/s.")
         }
 
     })
